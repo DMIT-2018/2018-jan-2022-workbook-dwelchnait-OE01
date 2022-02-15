fffc65169028b678622fe36e2fbd94afb42f08a6
Don Welch <dwelch@nait.ca>
2022-02-14 17:51:06 -0700

    Complete Controls page with drop down list, datalist-imput and the input range slider

--- src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -28,10 +28,50 @@ namespace WebApp.Pages.SamplePages
         [BindProperty]
         public string MessageText { get; set; }
 
+        [BindProperty]
+        public int MyRide { get; set; }
+        //pretend the collection is coming from the database
+        //each row of the collection has two values: selection value; selection text
+        //the class SelectionList will be use as the datatype for the collection
+        //the will need to be recreate on each internet trip
+        public List<SelectionList> Rides { get; set; }
+
+        [BindProperty]
+        public string VacationSpot { get; set; }
+        //pretend the collection is coming from the database
+        //create a List<string> to be used on the <datalist>
+        public List<string> VacationSpots { get; set; }
+
+        [BindProperty]
+        // input type=range
+        public int ReviewRating { get; set; }
         public void OnGet()
         {
+            PopulateLists();
         }
 
+        public void PopulateLists()
+        {
+            //used for the drop down list
+            Rides = new List<SelectionList>();
+            Rides.Add(new SelectionList() {ValueId=1, DisplayText="Car" });
+            Rides.Add(new SelectionList() { ValueId=2, DisplayText="Bus" });
+            Rides.Add(new SelectionList() { ValueId=3, DisplayText="Bike" });
+            Rides.Add(new SelectionList() { ValueId=4, DisplayText="Motorcycle" });
+            Rides.Add(new SelectionList() { ValueId=5, DisplayText="Board" });
+            Rides.Sort((x,y) => x.DisplayText.CompareTo(y.DisplayText)); //ascending
+            //Rides.Sort((x, y) => y.DisplayText.CompareTo(x.DisplayText)); //descending
+        
+            //used for datalist control
+            VacationSpots = new List<string>();
+            VacationSpots.Add("California");
+            VacationSpots.Add("Caribbean");
+            VacationSpots.Add("Cruising");
+            VacationSpots.Add("Europe");
+            VacationSpots.Add("Florida");
+            VacationSpots.Add("Mexico");
+
+        }
         public IActionResult OnPostText()
         {
             //echo back the input values
@@ -46,5 +86,22 @@ namespace WebApp.Pages.SamplePages
             return Page();
         }
 
+        public IActionResult OnPostListSlider()
+        {
+            //echo back the input values
+            Feedback = $"Ride {MyRide}; Vacation Spot {VacationSpot}; Control Review Rating {ReviewRating}";
+            //on each post, reload the lists
+            PopulateLists();
+            return Page();
+        }
+
+    }
+
+    public class SelectionList
+    {
+        //this class is designed for a <select> tag (dropdownlist) which needs a
+        //value item and a display item
+        public int ValueId { get; set; } //will be the returned value
+        public string DisplayText { get; set; } //will be the text display
     }
 }
