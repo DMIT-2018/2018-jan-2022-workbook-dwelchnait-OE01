463cb44dceacbb617b849933f6351a3ba4475afc
Don Welch <dwelch@nait.ca>
2022-03-23 17:41:08 -0600

    RE #3 create data models.

---
+++ src/ChinookSolution/ChinookSystem/ViewModels/PlaylistTrackMove.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class PlaylistTrackMove
+    {
+        public int TrackId { get; set; }
+        public int TrackNumber { get; set; }
+    }
+}
